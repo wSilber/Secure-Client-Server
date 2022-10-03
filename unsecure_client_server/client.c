00eabbeefea9c5b7fcebb8eada8aece300862784
wsilber <silberstein75@gmail.com>
2022-10-03 14:54:02 -0500

    Finished Unsecure Client and Server

---
+++ unsecure_client_server/client.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <unistd.h>
+
+#define PORT 12346
+#define BUFFER_SIZE 512
+
+#define OK 0
+#define NOT_OK 1
+
+struct sockaddr_in server_addr;
+char * address = "172.0.0.1";
+
+int main(int argc, char * * argv)
+{
+
+	int client_fd;
+	int client_socket;
+
+	char buffer[BUFFER_SIZE];
+
+	/* Create socket for client */	
+	if((client_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+	{
+		perror("Socket failed");
+		return (NOT_OK);
+	}
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = inet_addr(address);
+	server_addr.sin_port = htons(PORT);
+
+	/* Connect to server */
+	if((client_fd = connect(client_socket, 
+		   				    (struct sockaddr * ) & server_addr, 
+						    sizeof(server_addr))) < 0)
+	{
+		printf("Connection failed \n");
+		return (NOT_OK);
+	}
+
+	printf("Connected to server. Write your message to server...\n");
+
+	memset(buffer, '\0', BUFFER_SIZE);
+	
+	fgets(buffer, BUFFER_SIZE, stdin);
+	printf("Buffer: %s \n", buffer);
+	
+	/* Send message to server */
+	send(client_socket, buffer, BUFFER_SIZE, 0);
+
+	memset(buffer, '\0', BUFFER_SIZE);
+
+	/* Wait for response from server */
+	recv(client_socket, buffer, BUFFER_SIZE, 0);
+
+	printf("Server: %s\n", buffer);
+
+	/* Disconnect from server */
+	close(client_socket);
+
+    return (OK);
+}
---
+++ unsecure_client_server/server.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <errno.h>
+
+#define BUFFER_SIZE 512
+#define PORT 123456
+
+#define OK 0
+#define NOT_OK 1
+
+struct sockaddr_in server_addr, client_addr;
+char * address = "172.0.0.1";
+
+int main(void)
+{
+
+	/* Server/ Client socket handles */
+	int server_socket, client_socket;
+
+	/* Amount of queued connections allowed */
+	int backlog = 1;
+
+	/* Buffer to hold message for sending/ receiving */
+	char buffer_client[BUFFER_SIZE];
+	char buffer_server[BUFFER_SIZE];
+
+	/* Create socket */	
+	if((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+	{
+		perror("Socket failed");
+		return (NOT_OK);
+	}
+
+	server_addr.sin_family      = AF_INET;
+	server_addr.sin_addr.s_addr = inet_addr(address);
+	server_addr.sin_port        = htons(PORT);
+
+	/* Bind port number to the socket */
+	if(bind(server_socket, (struct sockaddr * ) & server_addr, sizeof(server_addr)) < 0)
+	{
+
+		perror("Bind failed");
+		return (NOT_OK);
+
+	}
+
+	printf("Done with binding with IP: %s, Port: %d\n", address, PORT);
+
+	/* Listen for connections */
+	if(listen(server_socket, backlog) == -1)
+	{
+		perror("Error listening");
+		return (NOT_OK);	
+	}
+
+	memset(buffer_client, 0, BUFFER_SIZE);
+
+	socklen_t client_addr_size = sizeof(client_addr);
+
+	if((client_socket = accept(server_socket, 
+                               (struct sockaddr * ) & client_addr,
+                               & client_addr_size)) < 0)
+		{
+			perror("Error accepting");
+			return (NOT_OK);
+		}
+
+	int client_ip = client_addr.sin_addr.s_addr;
+
+	/* Parse client ip address and port */
+	printf("Client connected at IP: %d.%d.%d.%d:%i\n", 
+			client_ip & 0xFF,
+			(client_ip >> 8) & 0xFF,
+			(client_ip >> 16) & 0xFF,
+			(client_ip << 24) & 0xFF,
+			ntohs(client_addr.sin_port));
+
+	printf("Client has connected\n");
+
+	/* Receive message from client */	
+	recv(client_socket, buffer_client, BUFFER_SIZE, 0);
+
+	printf("Client: %s \n", buffer_client);
+	memset(buffer_server, 0, BUFFER_SIZE);
+	
+	strcpy(buffer_server, "Received message from Client!");
+	printf("Server: %s \n", buffer_server);
+	
+	/* Send server response to client */
+	send(client_socket, buffer_server, BUFFER_SIZE, 0);
+	
+	/* Close connection to client */
+	close(client_socket);
+    close(server_socket);
+
+	return (OK);
+
+}
